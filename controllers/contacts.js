022ece1fda14bdb16e39a8811cc04d605b0850bb
ashleyka <ashleykamujoma@gmail.com>
2025-03-22 18:43:25 +0200

    Small changes..

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: userId });
+    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactsId });
     result.toArray().then((contacts) => {
             res.setHeader('Content-Type', 'application/json');
             res.status(200).json(contacts[0]);
